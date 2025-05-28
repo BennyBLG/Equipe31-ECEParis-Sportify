4c6c26822309d1000687a5b7291b49c92fa86bbb
Kvely <149174834+Kvely@users.noreply.github.com>
2025-05-28 13:52:04 +0200

    V6

--- accueil.php
+++ accueil.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sportify - Votre coach, votre rythme, votre succès</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
     <!-- Rectangle supérieur avec menu aligné -->
+
     <div class="top-bar">
         <div class="header-container">
             <h1 class="center-title">Sportify</h1>
